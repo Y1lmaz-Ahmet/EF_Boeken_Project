eeb626f61072c2c646a75035accf71ffda27e2b9
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-05-04 21:40:03 +0200

    aanmaken folder "Model' met 3 klassen.

---
+++ Boeken_Console/Model/Auteur.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boeken_Console.Model
+{
+    public class Auteur
+    {
+        public Auteur(string naam, string emailContact)
+        {
+            Naam = naam;
+            EmailContact = emailContact;
+        }
+
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public string EmailContact { get; set; }
+    }
+}
