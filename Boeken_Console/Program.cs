6f1779af41e032483f5331721ed12d8854e07fee
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-05-06 21:46:49 +0200

    aanmaken van dataTables met commando's.

--- Boeken_Console/Program.cs
+++ Boeken_Console/Program.cs
@@ -10,8 +10,23 @@ namespace Boeken_Console
     {
         static void Main(string[] args)
         {
-            //test
-            Console.WriteLine("hello world");
+            //-------- Add-Migration --------
+            //(venster) : VIEW > Other Windows > Package Manager Console.
+            //Na het uitvoeren vandeze instructie zal er een nieuwe
+            //folder verschijnen in het project met de naam Migrations.
+            //In deze folder vind je dan enerzijds de klasse terug met de naam die je aan de Add-Migration
+            //als parameter hebt meegegevenen anderzijds ook een klasse die verwijst naar je DbContext
+            //klasse met als achtervoegsel Snapshot.  In de eerste klasse vind je een beschrijving in code
+            //van je tabellen, de tweede geeft dehuidigetoestand weervan het model
+            //en zal bij nieuwe migrations (updates) steeds worden aangepast.
+            //Om nu het datamodel effectief aan te maken in de databank geef je het
+            //commando Update-Database in het Package Manager Consolevenster.
+
+
+            //-------- Update-Database --------
+            //Om nu het datamodel effectief aan te maken in de databank geef je
+            //het commando Update-Database in het Package Manager Consolevenster.
+
         }
     }
 }
